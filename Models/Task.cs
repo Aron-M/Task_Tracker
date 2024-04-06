453c95e620b3049b92cacd59eb71450c84858504
Aron-M <aron.moelich88@gmail.com>
2024-04-06 13:23:30 +0100

    adding category.cs, task.cs and user.cs into odels folder

---
+++ Models/Task.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class Task
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    // Add other properties as needed
+}
